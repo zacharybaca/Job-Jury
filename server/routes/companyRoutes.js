3db82220ff4b5babd2a41c11a01d2249ad304b68
Zachary Baca <zachary.baca@msn.com>
2026-02-18 09:50:37 -0600

    Update companyRoutes.js

--- server/routes/companyRoutes.js
+++ server/routes/companyRoutes.js
@@ -1,14 +1,12 @@
 import express from "express";
-import {
-  createCompany,
-  getCompanies,
-  getCompany
-} from "../controllers/companyController.js";
+import { createCompany, getCompanies, getCompany } from "../controllers/companyController.js";
+import { upload } from "../config/cloudinary.js";
 
 const router = express.Router();
 
-router.post("/companies", createCompany);
-router.get("/companies", getCompanies);
-router.get("/companies/:id", getCompany);
+// The upload middleware must stay here in the route definition
+router.post("/", upload.single("image"), createCompany);
+router.get("/", getCompanies);
+router.get("/:id", getCompany);
 
 export default router;
