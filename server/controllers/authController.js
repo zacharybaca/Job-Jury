bb6098b4e9a7550cdbbbff0e58a63afccf4e7ba4
Zachary Baca <zachary.baca@msn.com>
2026-02-18 09:51:08 -0600

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,11 +1,7 @@
-
 import User from "../models/User.js";
-import generateToken from "../utils/generateToken.js"; // We'll fill this utility next
+import generateToken from "../utils/generateToken.js";
 import asyncHandler from "express-async-handler";
 
-// @desc    Register new user
-// @route   POST /api/auth/register
-// @access  Public
 const registerUser = asyncHandler(async (req, res) => {
   const { username, email, password } = req.body;
 
@@ -17,14 +13,10 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error("User already exists");
   }
 
-  const user = await User.create({
-    username,
-    email,
-    password,
-  });
+  const user = await User.create({ username, email, password });
 
   if (user) {
-    generateToken(res, user._id); // Generate JWT and set cookie
+    generateToken(res, user._id);
     res.status(201).json({
       _id: user._id,
       username: user.username,
@@ -36,17 +28,12 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-// @desc    Auth user & get token
-// @route   POST /api/auth/login
-// @access  Public
 const loginUser = asyncHandler(async (req, res) => {
   const { email, username, password } = req.body;
-
   const user = await User.findOne({ email }) || await User.findOne({ username });
 
-  // Check if user exists AND password matches (method defined in Model)
   if (user && (await user.matchPassword(password))) {
-    generateToken(res, user._id); // Generate JWT and set cookie
+    generateToken(res, user._id);
     res.status(200).json({
       _id: user._id,
       username: user.username,
@@ -58,16 +45,11 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 });
 
-// @desc    Logout user
-// @route   POST /api/auth/logout
-// @access  Public
 const logoutUser = asyncHandler(async (req, res) => {
-  // Clear the cookie by setting it to expire immediately
   res.cookie("jwt", "", {
     httpOnly: true,
     expires: new Date(0),
   });
-
   res.status(200).json({ message: "User logged out" });
 });
 
