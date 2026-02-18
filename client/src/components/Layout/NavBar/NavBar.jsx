d51ff8e0c923cfbd400e70e3cef6d3ee3004824c
Zachary Baca <zachary.baca@msn.com>
2026-02-18 12:56:37 -0600

    Update NavBar.jsx

--- client/src/components/Layout/NavBar/NavBar.jsx
+++ client/src/components/Layout/NavBar/NavBar.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../../assets/job-jury-logo-minimal.jpg';
 import './nav-bar.css';
 
 const NavBar = () => {
   return (
     <nav className="main-nav">
       <div className="nav-container">
-        <Link to="/" className="nav-logo">
-          Job<span className="logo-accent">Jury</span>
+        {/* Logo Section */}
+        <Link to="/" className="nav-logo-link">
+          <img src={logo} alt="Job Jury Logo" className="nav-logo-img" />
         </Link>
 
+        {/* Links Section */}
         <ul className="nav-links">
-          <li>
-            <Link to="/" className="nav-item">Browse Companies</Link>
-          </li>
-          <li>
-            <Link to="/register-company" className="nav-item">Register Company</Link>
-          </li>
-          {/* Add more links here as your app grows */}
+          <li><Link to="/" className="nav-item">Browse Companies</Link></li>
+          <li><Link to="/register-company" className="nav-item">Register Company</Link></li>
         </ul>
       </div>
     </nav>
