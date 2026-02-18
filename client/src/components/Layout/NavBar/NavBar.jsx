0d9800eede5c0113b839cc3ca90a7e0786249336
Zachary Baca <zachary.baca@msn.com>
2026-02-18 08:22:07 -0600

    Update NavBar.jsx

--- client/src/components/Layout/NavBar/NavBar.jsx
+++ client/src/components/Layout/NavBar/NavBar.jsx
@@ -1,35 +1,25 @@
-import { Link, NavLink } from "react-router-dom";
-import "./nav-bar.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './nav-bar.css';
 
 const NavBar = () => {
   return (
-    <nav className="navbar">
-      <ul className="nav-links">
-        <li>
-          <NavLink
-            to="/"
-            className={({ isActive }) => (isActive ? "active-link" : "")}
-          >
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/login"
-            className={({ isActive }) => (isActive ? "active-link" : "")}
-          >
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/register"
-            className={({ isActive }) => (isActive ? "active-link" : "")}
-          >
-            Register
-          </NavLink>
-        </li>
-      </ul>
+    <nav className="main-nav">
+      <div className="nav-container">
+        <Link to="/" className="nav-logo">
+          Job<span className="logo-accent">Jury</span>
+        </Link>
+
+        <ul className="nav-links">
+          <li>
+            <Link to="/" className="nav-item">Browse Companies</Link>
+          </li>
+          <li>
+            <Link to="/register-company" className="nav-item">Register Company</Link>
+          </li>
+          {/* Add more links here as your app grows */}
+        </ul>
+      </div>
     </nav>
   );
 };
