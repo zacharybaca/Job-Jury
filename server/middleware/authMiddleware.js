2cfba5caff20e9e99ef8dc3505fde0a34f0491ef
Zachary Baca <zachary.baca@msn.com>
2026-02-18 09:52:59 -0600

    Update authMiddleware.js

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,30 +1,22 @@
-
-import jwt from "jsonwebtoken";
-import User from "../models/User.js"; // We need to create this model next!
-import asyncHandler from "express-async-handler";
+import jwt from 'jsonwebtoken';
+import asyncHandler from 'express-async-handler';
+import User from '../models/User.js';
 
 const protect = asyncHandler(async (req, res, next) => {
-  let token;
-
-  // Check if the "jwt" cookie exists
-  token = req.cookies.jwt;
+  let token = req.cookies.jwt;
 
   if (token) {
     try {
-      // Verify the token using your secret key
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-      // Find the user but exclude the password field
-      req.user = await User.findById(decoded.userId).select("-password");
-
-      next(); // Move to the next middleware/controller
+      req.user = await User.findById(decoded.userId).select('-password');
+      next();
     } catch (error) {
       res.status(401);
-      throw new Error("Not authorized, invalid token");
+      throw new Error('Not authorized, token failed');
     }
   } else {
     res.status(401);
-    throw new Error("Not authorized, no token");
+    throw new Error('Not authorized, no token');
   }
 });
 
