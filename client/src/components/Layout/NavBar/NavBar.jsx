bbc7f1332fcfafa3f80351ab44db03866023fec8
Zachary Baca <zachary.baca@msn.com>
2026-02-25 11:14:16 -0600

    Update NavBar.jsx

--- client/src/components/Layout/NavBar/NavBar.jsx
+++ client/src/components/Layout/NavBar/NavBar.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useAuth } from '../../../hooks/useAuth.js';
 import './nav-bar.css';
 
 const NavBar = () => {
+  const { user } = useAuth();
+
   return (
     <nav className="main-nav">
       <div className="nav-container">
@@ -13,25 +16,38 @@ const NavBar = () => {
 
         {/* Links Section */}
         <ul className="nav-links">
-          <li>
-            <Link to="/register" className="nav-item">
-              Sign Up
-            </Link>
-          </li>
-          <li>
-            <Link to="/login" className="nav-item">
-              Login
-            </Link>
-          </li>
+          {!user && (
+            <>
+              <li>
+                <Link to="/register" className="nav-item">
+                  Sign Up
+                </Link>
+              </li>
+              <li>
+                <Link to="/login" className="nav-item">
+                  Login
+                </Link>
+              </li>
+            </>
+          )}
+          {user && (
+            <li>
+              <Link to="/logout" className="nav-item">
+                Logout
+              </Link>
+            </li>
+          )}
           <li>
             <Link to="/" className="nav-item">
               Browse Companies
             </Link>
           </li>
           <li>
-            <Link to="/register-company" className="nav-item">
-              Register Company
-            </Link>
+            {user && (
+              <Link to="/register-company" className="nav-item">
+                Register Company
+              </Link>
+            )}
           </li>
         </ul>
       </div>
