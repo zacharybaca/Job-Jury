547d5af0f17341390fa80b57278d34d18598688a
Zachary Baca <zachary.baca@msn.com>
2026-02-25 07:00:41 -0600

    Create AuthProvider.jsx

---
+++ client/src/contexts/Auth/AuthProvider.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import { useFetcher } from '../hooks/useFetcher';
+import { AuthContext } from './AuthContext.jsx';
+
+
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const { fetcher } = useFetcher();
+
+  const checkUserAuth = async () => {
+    try {
+      const response = await fetcher('/api/users/me');
+      if (response.success) {
+        setUser(response.user);
+      } else {
+        setUser(null);
+      }
+    } catch (err) {
+      setUser(null);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    checkUserAuth();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, setUser, loading, checkUserAuth }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
