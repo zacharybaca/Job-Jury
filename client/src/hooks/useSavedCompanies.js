c85466fbb20e1fc7aacbb33713ac87079a129a18
Zachary Baca <zachary.baca@msn.com>
2026-03-02 08:51:24 -0600

    Add custom hook for accessing saved companies context

---
+++ client/src/hooks/useSavedCompanies.js
@@ -0,0 +1,10 @@
+import { useContext } from 'react';
+import { SavedCompaniesContext } from '../contexts/SavedCompanies/SavedCompaniesContext.jsx';
+
+export const useSavedCompanies = () => {
+  const context = useContext(SavedCompaniesContext);
+  if (!context) {
+    throw new Error('useSavedCompanies must be used within a SavedCompaniesProvider');
+  }
+  return context;
+};
