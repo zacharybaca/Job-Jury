bda397da5db9527397744433c2018ebdfbb4aea2
Zachary Baca <zachary.baca@msn.com>
2026-03-02 08:49:10 -0600

    Create SavedCompaniesProvider.jsx

---
+++ client/src/contexts/SavedCompanies/SavedCompaniesProvider.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import { useFetcher } from '../../hooks/useFetcher.js';
+import { SavedCompaniesContext } from './SavedCompaniesContext.jsx';
+
+export const SavedCompaniesProvider = ({ children }) => {
+  const [savedCompanies, setSavedCompanies] = useState([]);
+  const { fetcher } = useFetcher();
+
+  const fetchSavedCompanies = async () => {
+    try {
+      const response = await fetcher('/api/users/profile');
+
+      if (response.status === 401) {
+        setSavedCompanies([]); // User is not logged in, clear saved companies
+        return;
+      } else if (response.success) {
+        // Set saved companies from the nested data property
+        setSavedCompanies(response.data.savedCompanies);
+      }
+    } catch (error) {
+      console.error('Error fetching saved companies:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchSavedCompanies();
+  }, []);
+
+  return (
+    <SavedCompaniesContext.Provider value={{ savedCompanies, setSavedCompanies, fetchSavedCompanies }}>
+      {children}
+    </SavedCompaniesContext.Provider>
+  );
+};
