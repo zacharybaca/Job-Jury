ad2603cc19332959237b76865502176d1315c2fc
Zachary Baca <zachary.baca@msn.com>
2026-03-03 11:25:35 -0600

    Add admin middleware for authorization checks

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -33,4 +33,13 @@ const protect = asyncHandler(async (req, res, next) => {
   throw new Error("Not authorized, no token");
 });
 
-export { protect };
+const admin = (req, res, next) => {
+  if (req.user && (req.user.isAdmin || req.user.role === 'admin')) {
+    next();
+  } else {
+    res.status(403); // Forbidden
+    throw new Error("Not authorized as an admin");
+  }
+};
+
+export { protect, admin };
