9ab89cbe8846a41a71d16886d7774114487d51b3
Zachary Baca <zachary.baca@msn.com>
2026-03-04 13:16:11 -0600

    Update companyRoutes.js

--- server/routes/companyRoutes.js
+++ server/routes/companyRoutes.js
@@ -3,9 +3,10 @@ import {
   createCompany,
   getCompanies,
   getCompany,
+  deleteCompany,
 } from "../controllers/companyController.js";
 import { upload } from "../middleware/cloudinary.js";
-import { admin } from "../middleware/authMiddleware.js";
+import { admin, protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
@@ -13,5 +14,6 @@ const router = express.Router();
 router.post("/", upload.single("image"), createCompany);
 router.get("/", getCompanies);
 router.get("/:id", getCompany);
+router.delete("/:id", protect, admin, deleteCompany);
 
 export default router;
