e0fcb797cd69885c8ff9654033aeb56d47a58c6d
zacharybaca <thezachstack@pm.me>
2026-03-11 20:12:10 -0500

    finishing touches for deployment

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 const corsOptions = {
   // Allow requests from your Vite frontend
-  origin: process.env.CLIENT_URL || "http://localhost:5173",
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true, // Critical for cookies/sessions
 };
 
