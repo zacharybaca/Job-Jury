8e1e23211ee3f76129fb4e0d0b95fddf0f6305e6
Zachary Baca <zachary.baca@msn.com>
2026-03-13 10:15:01 -0500

    Create Avatar.jsx

---
+++ client/src/components/Layout/Avatar/Avatar.jsx
@@ -0,0 +1,14 @@
+import './avatar.css';
+
+const Avatar = ({ src, alt, size }) => {
+  return (
+    <img
+      className={`avatar avatar-${size}`}
+      src={src}
+      alt={alt}
+    />
+  );
+}
+
+export default Avatar;
+
