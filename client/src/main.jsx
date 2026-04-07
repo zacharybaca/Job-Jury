60c227aa5cb88a494486a68929b87bf338bd145e
Zachary Baca <zachary.baca@msn.com>
2026-04-07 08:39:40 -0500

    Import Bootstrap CSS for styling

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { AppProvider } from './contexts/AppProvider.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'modern-normalize';
 import './index.css';
 
