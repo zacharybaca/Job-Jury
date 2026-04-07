2daf18da80652a68e4fc02de477ad0fbb78b126e
Zachary Baca <zachary.baca@msn.com>
2026-04-07 10:24:17 -0500

    Integrate Carousel to display saved companies

--- client/src/components/Auth/MyFavorites/MyFavorites.jsx
+++ client/src/components/Auth/MyFavorites/MyFavorites.jsx
@@ -1,13 +1,27 @@
+import Carousel from 'react-bootstrap/Carousel';
+import ExampleCarouselImage from 'components/ExampleCarouselImage';
+import { useSavedCompanies } from '../../../contexts/SavedCompanies/SavedCompaniesContext.jsx';
 import './my-favorites.css';
 
 const MyFavorites = () => {
+    const { savedCompanies } = useSavedCompanies();
+
     return (
         <div className="my-favorites-container">
-            <h2>My Favorites</h2>
-            <p>This is where your favorite companies will be displayed.</p>
+            <Carousel>
+                {savedCompanies.map((company) => (
+                    <Carousel.Item key={company._id} interval={1000}>
+                        <ExampleCarouselImage text={company.name} />
+                        <Carousel.Caption>
+                            <h3>{company.name}</h3>
+                            <p>{company.industry} - {company.location}</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                ))}
+
+            </Carousel>
         </div>
     );
 }
 
 export default MyFavorites;
-
