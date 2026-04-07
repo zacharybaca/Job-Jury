f4ede0c95515edf0c881b9b90828336ef583592e
Zachary Baca <zachary.baca@msn.com>
2026-04-07 07:14:56 -0500

    Create MyFavorites.jsx

---
+++ client/src/components/Auth/MyFavorites/MyFavorites.jsx
@@ -0,0 +1,13 @@
+import './my-favorites.css';
+
+const MyFavorites = () => {
+    return (
+        <div className="my-favorites-container">
+            <h2>My Favorites</h2>
+            <p>This is where your favorite companies will be displayed.</p>
+        </div>
+    );
+}
+
+export default MyFavorites;
+
