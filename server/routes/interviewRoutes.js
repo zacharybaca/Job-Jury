4c17113428cdb972c3248f77910a42dba2d318b2
Zachary Baca <zachary.baca@msn.com>
2026-04-08 08:50:19 -0500

    Add interview routes for user and admin actions

---
+++ server/routes/interviewRoutes.js
@@ -0,0 +1,23 @@
+import express from "express";
+import {
+  createInterview,
+  getInterviewsByCompany,
+  getInterviewsByUser,
+  deleteInterview,
+  updateInterview
+} from "../controllers/interviewController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+// User Routes
+router.post("/", protect, createInterview);
+router.get("/company/:companyId", getInterviewsByCompany);
+router.get("/user/:userId", getInterviewsByUser);
+router.delete("/:id", protect, deleteInterview);
+router.put("/:id", protect, updateInterview);
+
+// Admin Routes
+router.get("/", protect, admin, getAllInterviews);
+
+export default router;
