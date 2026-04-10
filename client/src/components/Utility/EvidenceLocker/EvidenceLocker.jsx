f81a705d4076f611a007a1b15def1d8bd7c79696
Zachary Baca <zachary.baca@msn.com>
2026-04-10 08:54:38 -0500

    Add CSS import for LeakSubmissionForm
    Import leak submission form CSS for styling.

--- client/src/components/Utility/EvidenceLocker/EvidenceLocker.jsx
+++ client/src/components/Utility/EvidenceLocker/EvidenceLocker.jsx
@@ -7,6 +7,7 @@ import {
   Tooltip,
   ResponsiveContainer,
 } from 'recharts';
+import '../LeakSubmissionForm/leak-submission-form.css';
 
 const EvidenceLocker = ({ trends = [] }) => {
   console.log('Current Trends Data:', trends);
