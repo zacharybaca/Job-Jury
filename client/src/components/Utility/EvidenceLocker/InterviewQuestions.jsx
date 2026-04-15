e3d74013d67814127b4f8f0ac5d77c3b59b65a6c
Zachary Baca <zachary.baca@msn.com>
2026-04-15 09:12:24 -0500

    Refactor InterviewQuestions component to use Card

--- client/src/components/Utility/EvidenceLocker/InterviewQuestions.jsx
+++ client/src/components/Utility/EvidenceLocker/InterviewQuestions.jsx
@@ -1,15 +1,27 @@
+import Card from 'react-bootstrap/Card';
 
-
-const InterviewQuestions = ({ questions }) => {
+const InterviewQuestions = ({ questions, variant }) => {
 
   return (
     <div className="interview-questions">
       {questions && questions.length > 0 ? (
-        <ul>
-          {questions.map((question, index) => (
-            <li key={index}>{question}</li>
-          ))}
-        </ul>
+        <Card
+          bg={variant.toLowerCase()}
+          key={variant}
+          text={variant.toLowerCase() === 'light' ? 'dark' : 'white'}
+          style={{ width: '18rem' }}
+          className="mb-2"
+        >
+          <Card.Header>{variant} Interview Questions</Card.Header>
+          <Card.Body>
+            <Card.Title> Card Title </Card.Title>
+            <Card.Text>
+              {questions.map((question, index) => (
+                <div key={index}>{question}</div>
+              ))}
+            </Card.Text>
+          </Card.Body>
+        </Card>
       ) : (
         <p>No questions available.</p>
       )}
