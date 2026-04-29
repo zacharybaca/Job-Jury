b0f0718eaae49e5a6570a3183a6a53be7e94f15b
Zachary Baca <zachary.baca@msn.com>
2026-04-29 08:48:28 -0500

    Add custom profanity terms to moderationMiddleware
    Added custom profanity terms to the moderation middleware for improved content filtering.

--- server/middleware/moderationMiddleware.js
+++ server/middleware/moderationMiddleware.js
@@ -4,6 +4,51 @@ import asyncHandler from "express-async-handler";
 // Initialize default English dictionary
 leoProfanity.loadDictionary('en');
 
+// Append custom terms to the active memory array
+leoProfanity.add(['loser',
+  'idiot',
+  'dumb',
+  'ignorant',
+  'stupid',
+  'moron',
+  'fool',
+  'jerk',
+  'sociopath',
+  'slave driver',
+  'bloodsucker',
+  'brownnoser',
+  'kiss-ass',
+  'scam artist',
+  'crook',
+  'extortionist',
+  'nazi',
+  'fascist',
+  'psycho',
+  'bootlicker',
+  'shill',
+  'scab',
+  'brain-dead',
+  'sweatshop', "narcissist",
+  "megalomaniac",
+  "tyrant",
+  "dictator",
+  "parasite",
+  "leech",
+  "fraud",
+  "con artist",
+  "charlatan",
+  "backstabber",
+  "snake",
+  "creep",
+  "pervert",
+  "groomer",
+  "dirtbag",
+  "scum",
+  "sycophant",
+  "nepo baby",
+  "thief",
+  "embezzler"]);
+
 export const moderateContent = asyncHandler(async (req, res, next) => {
   const textToModerate = req.body.body || req.body.comment || req.body.content || req.body.text;
 
